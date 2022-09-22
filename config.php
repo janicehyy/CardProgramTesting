ffbfd02f892ea69e0760a511b0c6b01429ddd4cc
Janice Hoh <54612034+janicehyy@users.noreply.github.com>
2022-09-22 23:25:33 +0800

    Landing files

---
+++ config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://bursasharks.com/card/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://bursasharks.com/card/');
+
+// DIR
+define('DIR_LIVE', '/var/www/vhosts/bursasharks.com/httpdocs/card/');
+define('DIR_APPLICATION', '/var/www/vhosts/bursasharks.com/httpdocs/card/catalog/');
+define('DIR_SYSTEM', '/var/www/vhosts/bursasharks.com/httpdocs/card/system/');
+define('DIR_IMAGE', '/var/www/vhosts/bursasharks.com/httpdocs/card/image/');
+define('DIR_STORAGE', '/var/www/vhosts/bursasharks.com/httpdocs/card/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'janice_Tcard');
+define('DB_PASSWORD', 'ysg628K728%*@TW');
+define('DB_DATABASE', 'janice_card');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
