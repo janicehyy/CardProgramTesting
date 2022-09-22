0fe11eac1fffd0b975b409068a3d5565905b66a9
Janice Hoh <54612034+janicehyy@users.noreply.github.com>
2022-09-22 23:38:29 +0800

    Frontend Language

---
+++ catalog/language/en-gb/play/game.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']        = 'Card Game';
+
+// Text
+$_['text_title']        = 'Card Game';
+$_['text_agree']           = 'I have read and agree to the &nbsp;<a href="%s" class="agree"><b>%s</b></a>';
+$_['text_success']  		= '<p>Congratulations! Your new account has been successfully created!</p> <p>A confirmation email has been sent to the provided email address.</p>';
+
+// Entry
+$_['entry_people']      = 'No. of People';
+
+// Error
+$_['error_invalid']         = 'Warning: Invalid number. Please key in a valid number of people';
+$_['error_agree']          = 'Warning: You must agree to the %s!';
