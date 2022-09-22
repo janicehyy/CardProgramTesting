b65c37be057da95004783149d748f334fb4ca045
Janice Hoh <54612034+janicehyy@users.noreply.github.com>
2022-09-22 23:43:13 +0800

    Backend Language Files

---
+++ admin/language/en-gb/common/footer.php
@@ -0,0 +1,4 @@
+<?php
+// Text
+$_['text_footer']  = 'Copyrights &copy; Janice Hoh ' . date('Y') . '. All Rights Reserved.';
+$_['text_version'] = '';
---
+++ admin/language/en-gb/game/card.php
@@ -0,0 +1,19 @@
+<?php
+// Heading
+$_['heading_title']     = 'Card Game';
+
+// Text
+$_['text_success']      = 'Success: You have sucessfully deleted the record!';
+$_['text_list']         = 'Card Game List';
+$_['text_filter']       = 'Filter';
+		
+// Column
+$_['column_no_people']       = 'No. of People';
+$_['column_card']       = 'Cards';
+$_['column_date_added'] = 'Date Added';
+
+// Entry
+$_['entry_date_added']  = 'Date Added';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify record!';
